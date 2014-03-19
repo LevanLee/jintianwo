8d28cf3ca5e3f0e769e86ed5fa3a74b3f73d2a33
Levan <levanlifan@gmail.com>
2014-03-19 19:05:13 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jintianwo::Application.config.secret_key_base = 'cac6fed1c6318da5e794f84cfb99d90ea711ac87b6811cf9dae4fe52b5004a5269e05adc5846985288ab53bd5f35d23f2a23efa24c6cbd57e1946e3bfac083fc'
