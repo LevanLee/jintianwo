2a3a1d0ffacfe4eb15c32617e29d6309ea7cbbe6
tise.c <tise.c@gmail.com>
2014-03-28 17:49:41 +0800

    使用 username 登录

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,18 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:username]
+
+  validates_uniqueness_of :username
+  validates_presence_of :username
+  validates_presence_of :email
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:username)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
