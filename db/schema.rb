9f499b41fdfc0853b43a6954ffee133f9afe6ede
tise.c <tise.c@gmail.com>
2014-03-31 16:02:47 +0800

    取消 name and username 不重复设置

--- db/schema.rb
+++ db/schema.rb
@@ -47,8 +47,6 @@ ActiveRecord::Schema.define(version: 20140328084024) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["name"], name: "index_users_on_name", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
