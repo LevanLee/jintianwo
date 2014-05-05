d63e5d57d0003b1a31a84c38124474bd4c67d0df
tise.c <tise.c@gmail.com>
2014-05-05 20:01:22 +0800

    给 user_info 排序添加规则

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   def user_info
     num = 5
     @page_count = (Share.all.size / num.to_f).ceil
-    offset = (params[:page].to_i - 1) / num
-    @shares_page = Share.offset(offset).limit(num)
+    offset = (params[:page].to_i - 1) * num
+    @shares_page = Share.offset(offset).limit(num).order("created_at desc")
     user_info = render_to_string(:partial => "user_info", :formats => :html)
     render :json => { status: true, user_info: user_info}
   end
