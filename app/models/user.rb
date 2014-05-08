4c66d717b4481512578c98c003018f496d264b6f
tise.c <tise.c@gmail.com>
2014-05-08 15:04:13 +0800

    更改 users 表字段

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, :trackable, :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:username]
+         :recoverable, :rememberable, :validatable, :authentication_keys => [:username]
 
   validates_uniqueness_of :username
   validates_presence_of :username
--- db/schema.rb
+++ db/schema.rb
@@ -63,11 +63,8 @@ ActiveRecord::Schema.define(version: 20140429024920) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.integer  "like_count",             default: 0
+    t.integer  "deserve_count",          default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
